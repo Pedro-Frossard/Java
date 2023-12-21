640dfde434c7c61f868c03d1be3b48445268bba6
Pedro-Frossard <pedro.gomesfrossard@gmail.com>
2023-12-21 11:11:13 -0300

    primeiro ccommit

---
+++ Java/Casts.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class Casts {
+    public static void main(String[] args) {
+        double salario = 2700.5;
+        int valor = (int) salario;
+
+        long numeroGrande = 176425678276L;
+        short numeroPequeno = 32767; //maximo
+        byte numeroByte = 127; //maximo
+    }
+}
---
+++ Java/Variaveis.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade = 14;
+        String nome = "Pedro";
+        System.out.println("Olá, meu nome é "+ nome + " e eu tenho " + idade + " anos");
+        System.out.println("----------------------");
+        double num = 9.5;
+        System.out.println("Usando a palavra chave double para numeros quebrados --> " + num);
+    }
+}
